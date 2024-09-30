64deac263f293304b1842f8e516c951b7e152ba1
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-09-30 06:16:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Your User Name';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
